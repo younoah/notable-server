de7a0df43d10cf1d6c93c5e8ac271ad0117f191d
younoah <kr_uno_10@naver.com>
2022-02-02 21:41:15 +0900

    feat: 서버에 필요한 기본 미들웨어 추가

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
 
 app.use(express.json());
 app.get('/', (req, res) => {
